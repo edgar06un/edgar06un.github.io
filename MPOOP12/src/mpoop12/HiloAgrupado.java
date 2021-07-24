55b6bc6f20a1c21df598c730223148bc6e5c8b2a
Edgar Rivera <80140422+edgar06un@users.noreply.github.com>
2021-07-24 16:02:14 -0500

    Add files via upload

---
+++ MPOOP12/src/mpoop12/HiloAgrupado.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mpoop12;
+
+/**
+ *
+ * @author vsanchez
+ */
+public class HiloAgrupado extends Thread{
+
+    public HiloAgrupado(ThreadGroup tg, String string) {
+        super(tg, string);
+    }
+    @Override
+    public void run(){
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Iteracion "+i+" de "+getName());
+        }
+        System.out.println("Ha terminado el hilo "+getName());
+        
+    }
+   
+
+}
